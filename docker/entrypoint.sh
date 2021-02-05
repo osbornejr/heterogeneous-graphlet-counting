55c3451d917af71e9c69d8b441d4de2bbe0e3e7f
osbornejr <joelwilliamrobertson@gmail.com>
2021-02-05 06:21:31 +0000

    updating environment so that julia REPL has access to all processes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash --login
 set -e
 conda activate $ENV_PREFIX
+#set number of threads for julia to use.
+export JULIA_NUM_THREADS=$(nproc)
 #setup workspace
 #jupyter lab workspaces import workspace.json
 #set up password for notebook
