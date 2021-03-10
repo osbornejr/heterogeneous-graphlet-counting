68fc22d708f284c02c8a5ca7b90b42df7343ae63
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-03-10 07:51:06 +0000

    ready to run human smokers test

--- test/GSE68559.jl
+++ test/GSE68559.jl
@@ -13,18 +13,58 @@ data = Array(select(raw_counts,filter(x->occursin("data",x),names(raw_counts))))
 
 ## Clean - remove transcripts with total counts across all samples less than X
 X = 25
-raw_counts=raw_counts[vec(sum(data,dims = 2 ).>=X),:]
-data=Array(raw_counts[!,2:13]);
+norm_counts=raw_counts[vec(sum(data,dims = 2 ).>=X),:]
+data = Array(select(norm_counts,filter(x->occursin("data",x),names(norm_counts))))
 
-boxplot(raw_counts,"raw_data_cleaned_boxplot.svg")
+#boxplot(raw_counts,"raw_data_cleaned_boxplot.svg")
 
 ### Normalisation
 data=library_size_normalisation(data,"upperquartile")
-#PCs,D_1,per_sample=pca(data')
-#p=pca_plot(PCs,3);
-#draw(SVG("output/Construction/test_per_sample.svg"),p)
+norm_counts[findall(x->occursin("data",x),names(norm_counts))] = data
 
-## update data to normalised version
-#data=per_sample
-norm_counts=copy(raw_counts)
-norm_counts[!,2:13]=data
+##Sampling for most variable transcripts
+#add variance column to normalised data
+variance = vec(var(data, dims=2))
+norm_counts.variance = variance
+X = 0.01
+norm_counts_sample_noncoding=sort(norm_counts[norm_counts[:transcript_type].=="noncoding",:],:variance)[Int(round(end*(1-X))):end,:]
+norm_counts_sample_coding=sort(norm_counts[norm_counts[:transcript_type].=="coding",:],:variance)[Int(round(end*(1-X))):end,:]
+norm_counts_sample = outerjoin(norm_counts_sample_noncoding,norm_counts_sample_coding,on = names(norm_counts))
+data = Array(select(norm_counts_sample,filter(x->occursin("data",x),names(norm_counts_sample))))
+
+
+##Network construction
+#maintain list of vertices in graph
+vertexlist = norm_counts_sample[:transcript_type]
+##Measure of coexpression
+#similarity_matrix=mutual_information(data)
+similarity_matrix = coexpression_measure(data,"pearson")
+## Adjacency matrix
+threshold = 0.95
+pre_adj_matrix = adjacency(similarity_matrix,threshold)
+#Trim nodes with degree zero
+network_df = norm_counts_sample[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+data = data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+
+vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+adj_matrix = copy(pre_adj_matrix)
+adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
+adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+edgelist = edgelist_from_adj(adj_matrix)
+
+#Network Analysis
+degrees = sum(adj_matrix,dims=2)
+
+##set up for distributed mode
+#first clean to make sure there are no stray workers already around
+using Distributed
+rmprocs(workers())
+#add workers equal to the number of available cpus	
+addprocs(Threads.nthreads())
+#addprocs(8)
+@everywhere include(ENV["JULIA_PROJECT"]*"/src/GraphletCounting.jl")
+@everywhere include(ENV["JULIA_PROJECT"]*"/src/NullModel.jl")
+@time graphlet_counts = count_graphlets(vertexlist[:,2],edgelist,4,"distributed")
+#graphlet_concentrations = concentrate(graphlet_counts) 
+
+@time motif_counts = find_motifs(edgelist,"hetero_rewire",100, typed = true, typelist = vec(vertexlist),plotfile="test.svg",graphlet_size = 4)
