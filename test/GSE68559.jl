c00bad29ff37d579f7a1705f331284ead53efaf9
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-03-22 09:24:35 +0000

    adding network visualisation to human smoker dataset

--- test/GSE68559.jl
+++ test/GSE68559.jl
@@ -3,6 +3,19 @@ for src in filter(x->endswith(x,".jl"),readdir("src"))
 	include(ENV["JULIA_PROJECT"]*"/src/"*src)
 end
 samples = CSV.read.(filter(x->occursin(".txt",x),readdir("data/GSE68559_RAW",join=true)))
+##set up for distributed mode
+#first clean to make sure there are no stray workers already around
+using Distributed
+rmprocs(workers())
+#add workers equal to the number of available cpus	
+addprocs(Threads.nthreads())
+#addprocs(8)
+@everywhere include(ENV["JULIA_PROJECT"]*"/src/CoexpressionMeasures.jl")
+@everywhere include(ENV["JULIA_PROJECT"]*"/src/GraphletCounting.jl")
+@everywhere include(ENV["JULIA_PROJECT"]*"/src/NullModel.jl")
+
+
+##read in raw samples
 sample_names = replace.(filter(x->occursin(".txt",x),readdir("data/GSE68559_RAW")),"_isoforms_expr.txt"=>"").*" data"
 transcript_names=select(samples[1],1)
 raw_counts=rename!(hcat(transcript_names,select.(samples,Symbol("FPKM"))...,makeunique=true),["transcript_id";sample_names])
@@ -48,25 +61,22 @@ pre_adj_matrix = empirical_dist_adjacency(similarity_matrix,threshold)
 network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
 network_data = sample_data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
 
-vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0)]
 adj_matrix = copy(pre_adj_matrix)
 adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
 adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
 edgelist = edgelist_from_adj(adj_matrix)
 
+#Network visualisation
+using LightGraphs, GraphPlot
+g = SimpleGraph(adj_matrix)
+nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist.=="coding").+1]
+draw(SVG("GSE68559.svg",16cm,16cm),gplot(g,nodefillc = nodefillc))
+
 #Network Analysis
 degrees = sum(adj_matrix,dims=2)
 
-##set up for distributed mode
-#first clean to make sure there are no stray workers already around
-using Distributed
-rmprocs(workers())
-#add workers equal to the number of available cpus	
-addprocs(Threads.nthreads())
-#addprocs(8)
-@everywhere include(ENV["JULIA_PROJECT"]*"/src/GraphletCounting.jl")
-@everywhere include(ENV["JULIA_PROJECT"]*"/src/NullModel.jl")
-@time graphlet_counts = count_graphlets(vec(vertexlist),edgelist,4,"distributed")
+@time graphlet_counts = count_graphlets(vertexlist,edgelist,4,"distributed")
 #graphlet_concentrations = concentrate(graphlet_counts) 
 
 @time motif_counts = find_motifs(edgelist,"hetero_rewire",100, typed = true, typelist = vec(vertexlist),plotfile="test.svg",graphlet_size = 4)
