b8bb91dedc55dbcbcd282a744929db917607fb34
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-03-29 04:30:30 +0000

    updating both construction tests so that switching between different construction parameters is easier

--- test/Construction.jl
+++ test/Construction.jl
@@ -36,7 +36,8 @@ clean_data=Array(clean_counts[!,2:13]);
 
 
 ### Normalisation
-norm_data=library_size_normalisation(clean_data,"median")
+norm_method = "median"
+norm_data=library_size_normalisation(clean_data,norm_method)
 
 ## update data to normalised version
 #data=per_sample
@@ -60,80 +61,78 @@ sample_data=Array(sample_counts[!,2:13])
 ##Measure of coexpression
 #similarity_matrix=mutual_information(data)
 ## store similarity matrices for use later:
-methods =["pcit","mutual_information","pearson","PID"]
-SIMS_Mayank = Array{Array{Float64,2}}(undef,length(methods))
-
-for (i,method) in enumerate(methods)
-	@info "constructing network using $method method:"
-	coexpression = method
- 	similarity_matrix = coexpression_measure(sample_data,coexpression)
- 	SIMS_Mayank[i] = similarity_matrix
-## Adjacency matrix
- 	threshold = 0.99
+coexpression = "mutual_information"
+similarity_matrix = coexpression_measure(sample_data,coexpression)
+## Adjacency matrix (using empricial distribution method atm)
+threshold = 0.95
+threshold_method = "empirical_dist"
+if (threshold_method=="empirical_dist")
  	pre_adj_matrix = empirical_dist_adjacency(similarity_matrix,threshold)
-
-	#maintain list of vertices in graph
-	vertexlist = sample_counts[:transcript_type]
-	##Consensus measure method
-	#adj_matrix = consensus_measure(data,methods = ["pearson","spearman","kendall","mutual_information"])
-	
-	#Trim nodes with degree zero
-	network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	network_data = sample_data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	
-	vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0)]
-	adj_matrix = copy(pre_adj_matrix)
-	adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
-	adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	edgelist = edgelist_from_adj(adj_matrix)
-	
-	
-	###Sanity check... run the motif detection on an ALREADY random set of edges on the same node set
-	#sane_size = length(edgelist) 
-	#vertex_type_list = vec(vertexlist[:,2])
-	#rand1 = rand(1:size(vertexlist,1),sane_size)
-	#rand2 = rand(1:size(vertexlist,1),sane_size)
-	##first make sure there are no self loops
-	#sane_edgelist = splat(Pair).(eachrow(hcat(rand1[BitArray(((rand1.==rand2).-1).*-1)],rand2[BitArray(((rand1.==rand2).-1).*-1)])))
-	#
-	#now we order edges so that lower vertex label is first
-	#for (i,p) in enumerate(sane_edgelist)
-	#	if (first(p)>last(p))
-	#           sane_edgelist[i] = Pair(last(p),first(p))
-	#       	end
-	#end
-	##finally, get rid of multiedges
-	#sane_edgelist = unique(sane_edgelist)
-	#
-	
-	#get largest connected component TODO maybe have a way to select all components above a certain size, and plot them all separately?
-	#using LightGraphs
-	#using PrettyTables
-	g = Graph(adj_matrix) 
-	components = connected_components(g)
-	largest = components[length.(components).==max(length.(components)...)]
-	adj_matrix_comp = adj_matrix[largest[1],largest[1]]
-	g_comp = Graph(adj_matrix_comp)
-	##update vertexlist
-	vertexlist_comp = vertexlist[largest[1]]
-	##Network visualisation
-	#edgelist_comp=edgelist_from_adj(adj_matrix_comp)
-	#cytoscape_elements(vertexlist_comp,edgelist_comp,"cytoscape/elements.js")
-	#
-	#Network visualisation
-	using LightGraphs, GraphPlot
-	#g = SimpleGraph(adj_matrix)
-	
-	#network plot
-	nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist.=="coding").+1]
-	draw(SVG("Mayank_network_$X$coexpression.svg",16cm,16cm),gplot(g,nodefillc = nodefillc))
-	
-	#largest connected component plot
-	nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist_comp.=="coding").+1]
-	draw(SVG("Mayank_network_$X$coexpression component.svg",16cm,16cm),gplot(g_comp,nodefillc = nodefillc))
-	#Network Analysis
-	degrees = sum(adj_matrix,dims=2)
 end
+if (threshold_method=="hard")
+ 	pre_adj_matrix = adjacency(similarity_matrix,threshold)
+end
+## Adjacency matrix
+
+#maintain list of vertices in graph
+vertexlist = sample_counts[:transcript_type]
+##Consensus measure method
+#adj_matrix = consensus_measure(data,methods = ["pearson","spearman","kendall","mutual_information"])
+
+#Trim nodes with degree zero
+network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+network_data = sample_data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+
+vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0)]
+adj_matrix = copy(pre_adj_matrix)
+adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
+adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+edgelist = edgelist_from_adj(adj_matrix)
+
+
+###Sanity check... run the motif detection on an ALREADY random set of edges on the same node set
+#sane_size = length(edgelist) 
+#vertex_type_list = vec(vertexlist[:,2])
+#rand1 = rand(1:size(vertexlist,1),sane_size)
+#rand2 = rand(1:size(vertexlist,1),sane_size)
+##first make sure there are no self loops
+#sane_edgelist = splat(Pair).(eachrow(hcat(rand1[BitArray(((rand1.==rand2).-1).*-1)],rand2[BitArray(((rand1.==rand2).-1).*-1)])))
+#
+#now we order edges so that lower vertex label is first
+#for (i,p) in enumerate(sane_edgelist)
+#	if (first(p)>last(p))
+#           sane_edgelist[i] = Pair(last(p),first(p))
+#       	end
+#end
+##finally, get rid of multiedges
+#sane_edgelist = unique(sane_edgelist)
+#
+
+#get largest connected component TODO maybe have a way to select all components above a certain size, and plot them all separately?
+#using LightGraphs
+#using PrettyTables
+g = Graph(adj_matrix) 
+components = connected_components(g)
+largest = components[length.(components).==max(length.(components)...)]
+adj_matrix_comp = adj_matrix[largest[1],largest[1]]
+g_comp = Graph(adj_matrix_comp)
+##update vertexlist
+vertexlist_comp = vertexlist[largest[1]]
+##Network visualisation
+#edgelist_comp=edgelist_from_adj(adj_matrix_comp)
+#cytoscape_elements(vertexlist_comp,edgelist_comp,"cytoscape/elements.js")
+#
+#Network visualisation
+using LightGraphs, GraphPlot
+#g = SimpleGraph(adj_matrix)
+
+#network plot
+nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist_comp.=="coding").+1]
+draw(SVG("Mayank_$(norm_method)_$(threshold_method)_$(X)_$(coexpression)_component.svg",16cm,16cm),gplot(g_comp,nodefillc = nodefillc))
+nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist.=="coding").+1]
+draw(SVG("Mayank_$(norm_method)_$(threshold_method)_$(X)_$(coexpression).svg",16cm,16cm),gplot(g,nodefillc = nodefillc))
+#Network Analysis
+degrees = sum(adj_matrix,dims=2)
 #p = plot(DataFrame(sort(degrees,dims=1)),x = "x1",Geom.histogram,Guide.title("Degree Distribution"),Guide.xlabel("degree"));
 #draw(SVG("degree_distribution.svg"),p)
 #connected_components_html_table(adj_matrix,"cytoscape/connected_components.html")
--- test/GSE68559.jl
+++ test/GSE68559.jl
@@ -32,7 +32,8 @@ clean_data = Array(select(clean_counts,filter(x->occursin("data",x),names(clean_
 #boxplot(raw_counts,"raw_data_cleaned_boxplot.svg")
 
 ### Normalisation
-norm_data=library_size_normalisation(clean_data,"median")
+norm_method = "upper_quartile"
+norm_data=library_size_normalisation(clean_data,norm_method)
 norm_counts = copy(clean_counts)
 norm_counts[:,findall(x->occursin("data",x),names(norm_counts))] = norm_data
 
@@ -51,48 +52,49 @@ sample_data = Array(select(sample_counts,filter(x->occursin("data",x),names(samp
 ##Measure of coexpression
 #similarity_matrix=mutual_information(data)
 ## store similarity matrices for use later:
-methods =["pcit","mutual_information","pearson","PID"]
-SIMS_GSE = Array{Array{Float64,2}}(undef,length(methods))
 
-for (i,method) in enumerate(methods)
-	@info "constructing network using $method method:"
-	coexpression = method
- 	similarity_matrix = coexpression_measure(sample_data,coexpression)
- 	SIMS_GSE[i] = similarity_matrix
-	## Adjacency matrix (using empricial distribution method atm)
-	threshold = 0.99
-	pre_adj_matrix = empirical_dist_adjacency(similarity_matrix,threshold)
-	#maintain list of vertices in graph
-	vertexlist = sample_counts[:transcript_type]
-	#Trim nodes with degree zero
-	network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	network_data = sample_data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	
-	vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0)]
-	adj_matrix = copy(pre_adj_matrix)
-	adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
-	adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
-	edgelist = edgelist_from_adj(adj_matrix)
-	
-	#Network visualisation
-	using LightGraphs, GraphPlot
-	g = SimpleGraph(adj_matrix)
-	##get largest component
-	components = connected_components(g)
-	largest = components[length.(components).==max(length.(components)...)]
-	adj_matrix_comp = adj_matrix[largest[1],largest[1]]
-	g_comp = Graph(adj_matrix_comp)
-	##update vertexlist
-	vertexlist_comp = vertexlist[largest[1]]
-	##plot (either connected component or whole network
-	nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist_comp.=="coding").+1]
-	draw(SVG("GSE68559_$X$coexpression component.svg",16cm,16cm),gplot(g_comp,nodefillc = nodefillc))
-	nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist.=="coding").+1]
-	draw(SVG("GSE68559_$X$coexpression.svg",16cm,16cm),gplot(g,nodefillc = nodefillc))
-	
-	#Network Analysis
-	degrees = sum(adj_matrix,dims=2)
+coexpression = "PID"
+similarity_matrix = coexpression_measure(sample_data,coexpression)
+## Adjacency matrix (using empricial distribution method atm)
+threshold = 0.95
+threshold_method = "empirical_dist"
+if (threshold_method=="empirical_dist")
+ 	pre_adj_matrix = empirical_dist_adjacency(similarity_matrix,threshold)
 end
+if (threshold_method=="hard")
+ 	pre_adj_matrix = adjacency(similarity_matrix,threshold)
+end
+#maintain list of vertices in graph
+vertexlist = sample_counts[:transcript_type]
+#Trim nodes with degree zero
+network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+network_data = sample_data[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+
+vertexlist = vertexlist[vec(sum(pre_adj_matrix,dims=2).!=0)]
+adj_matrix = copy(pre_adj_matrix)
+adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
+adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+edgelist = edgelist_from_adj(adj_matrix)
+
+#Network visualisation
+using LightGraphs, GraphPlot
+g = SimpleGraph(adj_matrix)
+##get largest component
+components = connected_components(g)
+largest = components[length.(components).==max(length.(components)...)]
+adj_matrix_comp = adj_matrix[largest[1],largest[1]]
+g_comp = Graph(adj_matrix_comp)
+##update vertexlist
+vertexlist_comp = vertexlist[largest[1]]
+##plot (either connected component or whole network
+nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist_comp.=="coding").+1]
+draw(SVG("GSE68559_$(norm_method)_$(threshold_method)_$(X)_$(coexpression)_component.svg",16cm,16cm),gplot(g_comp,nodefillc = nodefillc))
+nodefillc = [colorant"lightseagreen", colorant"orange"][(vertexlist.=="coding").+1]
+draw(SVG("GSE68559_$(norm_method)_$(threshold_method)_$(X)_$(coexpression).svg",16cm,16cm),gplot(g,nodefillc = nodefillc))
+
+#Network Analysis
+degrees = sum(adj_matrix,dims=2)
+
 @time graphlet_counts = count_graphlets(vertexlist,edgelist,4,"distributed")
 #graphlet_concentrations = concentrate(graphlet_counts) 
 
