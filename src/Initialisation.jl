c3c93b9c6b9e8d8a55f5bcf3f19b0b7d5fb3e88f
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-05-04 09:31:45 +0000

    adding standalone file to initialise all julia files

---
+++ src/Initialisation.jl
@@ -0,0 +1,20 @@
+### Include all source files TODO make this occur more fluently and automatically by creating a package, and using Revise
+cwd = ENV["JULIA_PROJECT"]
+for src in filter(x->endswith(x,".jl"),readdir("src"))
+	if(src!="Initialisation.jl")
+		includet("$cwd/src/"*src)
+	end
+end
+
+#set up distributed workers
+#first clean to make sure there are no stray workers already around
+if(length(workers())!=Threads.nthreads())
+	rmprocs(workers())
+ 	#add workers equal to the number of available cpus	
+	addprocs(Threads.nthreads())
+ 	#addprocs(8)
+	@everywhere include("src/CoexpressionMeasures.jl")
+	@everywhere include("src/GraphletCounting.jl")
+	@everywhere include("src/NullModel.jl")
+
+end
