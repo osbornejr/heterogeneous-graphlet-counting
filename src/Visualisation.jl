bc9f472196cf027e86553e444707c0e5b674a88e
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-05-27 08:13:11 +0000

    adding basic DataFrame to html table writing function

--- src/Visualisation.jl
+++ src/Visualisation.jl
@@ -1,3 +1,4 @@
+using Printf
 function cytoscape_elements(vertices::Array{String,2},edges::Array{Pair},output_path::String)
 	io = open(output_path, "w")
 	println(io, "var Elements = {")
@@ -16,4 +17,31 @@ function cytoscape_elements(vertices::Array{String,2},edges::Array{Pair},output_
 	close(io)
 end
 
-	
+function html_table_maker(dataframe::DataFrame,outfile::String)
+	#begin table
+	#create header from dataframe names
+	table = "<table>\n<thead>\n<tr>\n"
+	for n in names(dataframe)
+		table = table*"<th>$n</th>\n"
+	end
+	table = table*"</tr>\n</thead>\n"
+	##body from rows
+	table = table*"<tbody>\n"
+	for r in eachrow(dataframe)
+
+	table = table*"<tr>\n"
+		for d in r
+			if (typeof(d)<:AbstractFloat)
+				table = table*"<td>$(@sprintf "%.5g" d)</td>\n"
+			else
+				table = table*"<td>$(d)</td>\n"
+			end
+		end
+	table = table*"</tr>\n"
+	end
+	table = table*"</tbody>\n</table>"
+	write(outfile,table)
+
+
+end
+
