312fcd182925afe1990e970529112ce977206586
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-05-27 08:14:28 +0000

    updating parameters

--- test/GSE68559.jl
+++ test/GSE68559.jl
@@ -1,4 +1,4 @@
-params = RunParameters("GSE68559","menu1","$cwd/website",1,"upper_quartile",0.01,"pidc",0.95,"empirical_dist",true,true,false)
+params = RunParameters("GSE68559","menu1","$cwd/website",25,"upper_quartile",0.01,"pidc",0.95,"empirical_dist",true,false,true)
 
 #Read in raw counts (cached)
 raw_counts_file = "$cwd/output/cache/$(params.test_name)_raw_counts.jld"
