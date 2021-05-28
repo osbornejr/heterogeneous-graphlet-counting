963d6050efe1ec4377483d86fc0babb8cac82987
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-05-28 13:39:57 +0000

    adding function to generate html table for type representation results (could be generalised to any graphlet dataframe information

--- src/Visualisation.jl
+++ src/Visualisation.jl
@@ -17,31 +17,45 @@ function cytoscape_elements(vertices::Array{String,2},edges::Array{Pair},output_
 	close(io)
 end
 
-function html_table_maker(dataframe::DataFrame,outfile::String)
-	#begin table
-	#create header from dataframe names
-	table = "<table>\n<thead>\n<tr>\n"
-	for n in names(dataframe)
-		table = table*"<th>$n</th>\n"
-	end
-	table = table*"</tr>\n</thead>\n"
-	##body from rows
-	table = table*"<tbody>\n"
-	for r in eachrow(dataframe)
-
-	table = table*"<tr>\n"
+function html_table_maker(dataframe::DataFrame,imgs::Array{String,1},outfile::String)
+	#begin table container with scoped styling
+	table ="""<div id="table container">\n\t<style type="text/css" scoped>\n\t\t.row {\n\t\t\tmargin-left:-5px;\n\t\t\tmargin-right:-5px;\n\t\t}\n\t\t.column {\n\t\t\tfloat: left;\n\t\t\twidth: 50%;\n\t\t}\n\t\ttd {\n\t\t\ttext-align:center;\n\t\t\tvertical-align:middle;\n\t\t}\n\t</style>\n\t<div class="row">\n\t\t<div class="column">\n"""
+	for (i,r) in enumerate(eachrow(dataframe))
+		##start new column after every 5 rows
+		if (mod(i,5)==1)
+			#close previous column
+			if(i>1)
+				table = table*"""\t\t\t\t</tbody>\n\t\t\t</table>\n\t\t</div>\n\t\t<div class="column">\n"""		 
+			end
+			#begin table...create header from dataframe names
+			table = table*"\t\t\t<table>\n\t\t\t\t<thead>\n\t\t\t\t\t<tr>\n"
+			for n in names(dataframe)
+				table = table*"\t\t\t\t\t\t<th>$n</th>\n"
+			end
+			table = table*"\t\t\t\t\t</tr>\n\t\t\t\t</thead>\n"
+			##body for next 5 rows
+			table = table*"\t\t\t\t<tbody>\n"
+		end
+		##begin row entry
+		table = table*"\t\t\t\t\t<tr>\n"
 		for d in r
+			# format floats and ints	
 			if (typeof(d)<:AbstractFloat)
-				table = table*"<td>$(@sprintf "%.5g" d)</td>\n"
+				table = table*"\t\t\t\t\t\t<td>$(@sprintf "%.5g" d)</td>\n"
+			# add image to table (asssumes image is located at figs/$d.svg)
+			elseif (d in imgs)
+				table = table*"""\t\t\t\t\t\t<td><img  width=80% src="./figs/$d.svg" title="$d"/></td>\n"""				
+				
 			else
-				table = table*"<td>$(d)</td>\n"
+				table = table*"\t\t\t\t\t\t<td>$(d)</td>\n"
 			end
 		end
-	table = table*"</tr>\n"
+		table = table*"\t\t\t\t\t</tr>\n"
 	end
-	table = table*"</tbody>\n</table>"
+	#close last table
+	table = table*"\t\t\t\t</tbody>\n\t\t\t</table>\n"
+ 	#close columns, rows and container
+	table = table*"\t\t</div>\n\t</div>\n</div>"
 	write(outfile,table)
-
-
 end
 
