8d35d5d9b504919c20c286872f0c3cc8fcf2dea8
Joel Robertson <joelwilliamrobertson@gmail.com>
2021-07-07 14:43:32 +0000

    adding function to plot generic threejs network

--- src/Visualisation.jl
+++ src/Visualisation.jl
@@ -68,3 +68,51 @@ function html_table_maker(dataframe::DataFrame,outfile::String;imgs::Array{Strin
 	write(outfile,table)
 end
 
+function threejs_plot(adj_matrix::AbstractArray,vertex_names::Array{String,1},colour_by::Array{String,1},plot_prefix::String,colours::Array{String,1})
+	R"""
+	sapply(names(sessionInfo()$otherPkgs),function(pkg) detach(paste0('package:',pkg),character.only =T,force = T));rm(list=ls())
+	"""
+	@rput adj_matrix
+	@rput vertex_names
+	@rput colour_by
+ 	@rput plot_prefix	
+	@rput colours
+	R"""
+	library(RColorBrewer)
+	library(igraph)
+	library(threejs)
+	library(htmlwidgets)
+	#keep this last to mask other packages:
+	library(tidyverse)
+	
+	##change to tibble
+	vertex_names = tibble(vertex_names)
+	vertex_names_trimmed = sapply(vertex_names,tools::file_path_sans_ext)
+	pre_g <- graph.adjacency(adj_matrix,mode="undirected",diag=F)
+	#Now we add attributes to graph by rebuilding it via edge and vertex lists
+	edges <- as_tibble(as.data.frame(get.edgelist(pre_g)))
+	vertices <- tibble(name = 1:nrow(vertex_names),vertex_names)%>% dplyr::rename(label=vertex_names)
+	vertices <- vertices %>% mutate(color = as_factor(colour_by))
+	g <- graph_from_data_frame(edges,directed = F, vertices)
+	#
+	##delete zero degree vertices
+	g <- delete.vertices(g,igraph::degree(g)==0)
+	##find maximum connected component
+	#g <- decompose(g, mode = "strong", max.comps = NA, min.vertices = 10)[[1]]
+	#
+	##get only connected vertices
+	#vertices <- vertices %>% filter(name %in% names(V(g)))
+	
+	##add colours to graph
+	#colour_palette = brewer.pal(name = "Spectral", n = length(levels(vertices$color)))
+	colour_palette = colours
+	levels(vertices$color) <- colour_palette
+	edges <- as_tibble(as.data.frame(get.edgelist(g)))
+	g <- graph_from_data_frame(edges,directed = F, vertices)
+	vertex_attr(g,"size") <- 0.5
+	plot = graphjs(g,bg = "white");
+	saveWidget(plot,paste0(plot_prefix,"_threejs_plot.html"))
+	"""
+
+	@info "threejs plot saved as HTML page at $(plot_prefix)."
+end
