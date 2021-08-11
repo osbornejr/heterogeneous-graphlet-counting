662ec10112e6dffcdbb7ebbb5353b0e026992c29
osbornejr <joelwilliamrobertson@gmail.com>
2021-08-11 16:06:38 +1000

    Updating tex boxplots to save in shared output directory.

--- src/Construction.jl
+++ src/Construction.jl
@@ -362,7 +362,7 @@ function webpage_construction(raw_counts::DataFrame,params::RunParameters)
                         #add real log values to summaries, order from lowest to highest)
                         summaries.values = log_real_fil.value
                         sort!(summaries,:values)
-                        tex_boxplot(summaries[!,Not(:values)],summaries.values,"$hog.tex","standalone",ylabel="log value")
+                        tex_boxplot(summaries[!,Not(:values)],summaries.values,"output/share/$(hog)_boxplot.tex","input",ylabel="")
 
                         hog_array[i] = hog_df
                         hog_array_under[i] = hog_df_under
