04c268d04746e0e261609c852d9f017d93e18fdb
osbornejr <joelwilliamrobertson@gmail.com>
2021-12-07 20:22:28 +1100

    Updating test to JLD2

--- test/GSE68559.jl
+++ test/GSE68559.jl
@@ -1,9 +1,9 @@
 params = RunParameters("GSE68559","menu1","$cwd/website",25,"upper_quartile",0.025,"pidc",0.95,"empirical_dist_zero",1000,true,false,true,true)
 
 #Read in raw counts (cached)
-raw_counts_file = "$cwd/output/cache/$(params.test_name)_raw_counts.jld"
+raw_counts_file = "$cwd/output/cache/$(params.test_name)_raw_counts.jld2"
 if (isfile(raw_counts_file))
-	raw_counts = JLD.load(raw_counts_file,"raw counts")
+    raw_counts = cache_load(raw_counts_file,"raw counts")
 else
 	samples = CSV.read.(filter(x->occursin(".txt",x),readdir("$cwd/data/GSE68559_RAW",join=true)))
 	sample_names = replace.(filter(x->occursin(".txt",x),readdir("$cwd/data/GSE68559_RAW")),"_isoforms_expr.txt"=>"").*" data"
@@ -12,7 +12,7 @@ else
 	gene_short_names = select(samples[1],5)
 	raw_counts = rename!(hcat(transcript_names,select.(samples,Symbol("FPKM"))...,makeunique=true,gene_names,gene_short_names),["transcript_id";sample_names;"gene_id";"gene_short_id"])
 	raw_counts.transcript_type = replace(x-> occursin("lnc",x) ? "noncoding" : "coding",raw_counts.transcript_id)
-	JLD.save(raw_counts_file,"raw counts",raw_counts)
+	cache_save(raw_counts_file,"raw counts"=>raw_counts)
 end
 
 ## find transcript types via biomaRt (NOTE this should be inside the raw counts cache section above, but currently input data has vanished from NeCTAR! It is all cached here thankfully, so for now we use this workaround.
@@ -21,9 +21,9 @@ biomart_modification = true
 
 
 if(biomart_modification = true)
-	biomart_raw_counts_file = "$cwd/output/cache/$(params.test_name)_raw_counts_biomart.jld"
+	biomart_raw_counts_file = "$cwd/output/cache/$(params.test_name)_raw_counts_biomart.jld2"
 	if (isfile(biomart_raw_counts_file))
-		raw_counts = JLD.load(biomart_raw_counts_file,"raw counts")
+		raw_counts = cache_load(biomart_raw_counts_file,"raw counts")
 	else
 		#restart R session	
 		R"""
@@ -60,7 +60,7 @@ if(biomart_modification = true)
 		raw_counts = vcat(filter(:transcript_type=>x->x=="noncoding",raw_counts),filter(:biomart_type=>x->x in ["lncRNA","protein_coding"],raw_counts))
 		## set transcripts that biomart identifies as lncRNA to noncoding
 		raw_counts.transcript_type = @. ifelse(raw_counts.biomart_type=="lncRNA","noncoding",raw_counts.transcript_type)
-		JLD.save(biomart_raw_counts_file,"raw counts",raw_counts)
+		cache_save(biomart_raw_counts_file,"raw counts"=>raw_counts)
 	end
 end
 ##run code to generate plots and figures for website 
