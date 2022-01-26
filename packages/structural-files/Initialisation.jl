b8fb03f4ff2287f3f1946446c1280730060495f9
osbornejr <joelwilliamrobertson@gmail.com>
2022-01-26 23:28:35 +1100

    removing RunParameters struct from Initialisation.jl

--- packages/structural-files/Initialisation.jl
+++ packages/structural-files/Initialisation.jl
@@ -2,23 +2,6 @@
 using Revise
 ### Include all source files TODO make this occur more fluently and automatically by creating a package, and using Revise
 cwd = ENV["JULIA_PROJECT"]
-#define structure for run_parameters (nb... at this stage has to be rerun if values change). Also needs to be defined before including any package that depends on it
-struct RunParameters
-    test_name::String   
-    page_name::String
-    website_dir::String
-    expression_cutoff::Int
-    norm_method::String
-    variance_percent::Float64
-    coexpression::String
-    threshold::Float64
-    threshold_method::String
-    null_model_size::Int
-    func_annotate::Bool
-    visualise::Bool
-    graphlet_counting::Bool
-    wgcna::Bool
-end
 
 
 for src in filter(x->endswith(x,".jl"),readdir("src"))
