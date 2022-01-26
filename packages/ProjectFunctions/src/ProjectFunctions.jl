7c33c22d9b4fc92b42503a9ff6566b6b92d13744
osbornejr <joelwilliamrobertson@gmail.com>
2022-01-26 23:26:17 +1100

    adding function to add and code load worker processes

--- packages/ProjectFunctions/src/ProjectFunctions.jl
+++ packages/ProjectFunctions/src/ProjectFunctions.jl
@@ -1,10 +1,10 @@
 module ProjectFunctions
-export cache_save, cache_load,  @name
-using DataPreprocessing, NetworkConstruction,GraphletCounting,GraphletAnalysis
+export cwd,cache_save, cache_load,  @name
+using DataPreprocessing, NetworkConstruction,GraphletCounting ,GraphletAnalysis
 
 include("RunFunctions.jl")
 
-cwd = ENV["JULIA_PROJECT"]
+cwd = ENV["PWD"]
 #define structure for run_parameters (nb... at this stage has to be rerun if values change). Also needs to be defined before including any package that depends on it
 #struct RunParameters
 #    test_name::String   
@@ -23,12 +23,46 @@ cwd = ENV["JULIA_PROJECT"]
 #    wgcna::Bool
 #end
 
+function distributed_setup(inclusions::Vector{Symbol})
+    distributed_setup(inclusions...)
+end
+
+function distributed_setup(inclusions::Symbol...)
+
+    ##set up for distributed mode
+
+    #first clean to make sure there are no stray workers already around
+    rmprocs(workers())
+    #add workers equal to the number of available cpus      
+    addprocs(Threads.nthreads();exeflags="--project=$cwd")
+    #addprocs(8)
+    #@everywhere inclusions
+    for inc in inclusions
+        eval(macroexpand(Distributed,quote @everywhere using $(inc) end))
+    end
+end
+
+function distributed_code_load(inclusions::Symbol...)
+    for package in inclusions
+        eval(macroexpand(Distributed,quote @everywhere using $(package) end))
+    end
+end
+
 macro name(arg)
     x = string(arg)
     quote
         $x
     end
 end
+
+#aggregator function
+function t2(d1,d2)
+    append!(d1,d2)
+    d1
+end
+
+##allows a function to be broadcast and splat each argument
+splat(f) = args->f(args...)
 
 function cache_save(file_name::String,save_objects::Array{<:Pair,1})
     ##shorthand way to use JLD2 method to save files to disk
