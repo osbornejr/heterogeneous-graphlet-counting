857bd985a800760584e10d62fc214b39e906ba87
osbornejr <joelwilliamrobertson@gmail.com>
2022-05-31 12:28:38 +1000

    changing startup to include infiltrator (and remove sticky shell workaround)

--- config/startup-files/env-startup.jl
+++ config/startup-files/env-startup.jl
@@ -1,14 +1,7 @@
-import REPL
-Base.atreplinit() do repl
-	    # make the ; shell mode sticky
-	         	!isdefined(repl, :interface) && (repl.interface = REPL.setup_interface(repl))
-	             	repl.interface.modes[2].sticky=true
-	          end
-#using Weave
-#using Debugger
 using Revise
+using Infiltrator
 using Pkg
 using Distributed
-using ProjectFunctions
+#using ProjectFunctions
 #Pkg.precompile()
 #ENV["JULIA_PROJECT"] = "/home/osbornejr/app"
