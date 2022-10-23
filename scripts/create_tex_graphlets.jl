1781451371b2e0f11ed0b01e134fcace393f59ec
osbornejr <joelwilliamrobertson@gmail.com>
2022-10-23 13:14:35 +1100

    script to generate tex graphlets updated with correct orbits

--- scripts/create_tex_graphlets.jl
+++ scripts/create_tex_graphlets.jl
@@ -8,9 +8,10 @@ orbits = Dict{String,Vector{Int}}("2-path"=>[1,1],
                                  "4-star"=>[1,1,2,1],
                                  "4-tail"=>[1,1,2,3],
                                  "4-cycle"=>[1,2,1,2],
-                                 "4-chord"=>[1,2,2,1],
+                                 "4-chord"=>[1,2,1,2],
                                  "4-clique"=>[1,1,1,1]
                                 )
+###note that the above orbit input for 4-cycle is technically incorrect, but it allows us to generate in this case the sole(?) het graphlet that is not implied by orbit structure. It also produces 3 other non-unique-symmetry 4-cycle graphlets that can be ignored.  
 #generate list of all required graphlets
 List = String[]
 for (g,o) in orbits
@@ -25,7 +26,7 @@ end
 out = ENV["PWD"]*"/output/share/graphlets/" 
 run(`mkdir -p $(out)`)
 for g in List
-    ## colours need to be reversed if graphlet is all noncoding TODO make work for any number of types
+    ## colours need to be reversed if graphlet is all noncoding TODO currently only works if number of types is 2; make work for any number of types
     if ((!occursin("_coding",g)) & (!startswith(g,"coding")))
         NetworkConstruction.draw_tex_graphlet(g,colours=reverse(colours),out_file = out*g*".tex")
     else
