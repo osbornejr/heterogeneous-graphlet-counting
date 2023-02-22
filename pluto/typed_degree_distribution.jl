423256f88289a1e8ff100593823111784bd573aa
osbornejr <joelwilliamrobertson@gmail.com>
2023-02-22 17:20:14 +1100

    up[dating pluto files

--- pluto/typed_degree_distribution.jl
+++ pluto/typed_degree_distribution.jl
@@ -59,12 +59,19 @@ data = DataFrame(coding = map(x->DefaultDict(0,x)["coding"],dd),noncoding = map(
 # ╔═╡ db25a7ae-26b0-40da-97d3-06edb70c70d9
 bc = 50
 
-# ╔═╡ bd3d754d-9687-4ac2-bccc-b16fca7dfcf5
+# ╔═╡ 53770902-0cee-4880-b480-e475e158a369
+# ╠═╡ show_logs = false
 plot(
 	layer(data[vertexlist.=="coding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["coding->coding"]),
 	layer(data[vertexlist.=="coding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["coding->noncoding"]),
 	layer(data[vertexlist.=="noncoding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->coding"]),
-	layer(data[vertexlist.=="noncoding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->noncoding"]))
+	layer(data[vertexlist.=="noncoding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->noncoding"]),
+	Guide.title("Typed degree distribution"),
+	Guide.xlabel("degree"),
+	Guide.ylabel("frequency"))
+
+# ╔═╡ 59a53882-786f-44ee-9607-c93b65db9635
+
 
 # ╔═╡ 0502e056-3059-4941-84d0-fe7da897bc48
 data[vertexlist.=="coding",:]
@@ -75,6 +82,7 @@ data[vertexlist.=="coding",:]
 # ╠═170965d2-0728-4baf-950e-f2b56739427d
 # ╠═02211a8a-87a4-4bd5-acf8-74cd27b902c6
 # ╠═5e6beb8d-47fc-4a1b-926e-ae17a51b2ee4
-# ╠═bd3d754d-9687-4ac2-bccc-b16fca7dfcf5
 # ╠═db25a7ae-26b0-40da-97d3-06edb70c70d9
+# ╠═53770902-0cee-4880-b480-e475e158a369
+# ╠═59a53882-786f-44ee-9607-c93b65db9635
 # ╠═0502e056-3059-4941-84d0-fe7da897bc48
