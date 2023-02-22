2fa0c7ad04f24b733d2879bf0cedf056794c0dbd
osbornejr <joelwilliamrobertson@gmail.com>
2023-02-22 12:15:18 +1100

    updating histo plot on pluto doc

--- pluto/typed_degree_distribution.jl
+++ pluto/typed_degree_distribution.jl
@@ -56,11 +56,15 @@ dd = GraphletCounting.typed_degree_distribution(vertexlist,edgelist);
 # ╔═╡ 5e6beb8d-47fc-4a1b-926e-ae17a51b2ee4
 data = DataFrame(coding = map(x->DefaultDict(0,x)["coding"],dd),noncoding = map(x->DefaultDict(0,x)["noncoding"],dd))
 
-# ╔═╡ bd3d754d-9687-4ac2-bccc-b16fca7dfcf5
-plot(data[vertexlist.=="noncoding",:], x = "noncoding",Geom.histogram,Guide.title("Typed degree distribution"),style(point_size = 0.4mm))
+# ╔═╡ db25a7ae-26b0-40da-97d3-06edb70c70d9
+bc = 50
 
-# ╔═╡ 732e425b-6549-48c5-bf8c-e196d977356b
-plot(data[vertexlist.=="noncoding",:], x = "noncoding",Geom.histogram,Guide.title("Typed degree distribution"),style(point_size = 0.4mm))
+# ╔═╡ bd3d754d-9687-4ac2-bccc-b16fca7dfcf5
+plot(
+	layer(data[vertexlist.=="coding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["coding->coding"]),
+	layer(data[vertexlist.=="coding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["coding->noncoding"]),
+	layer(data[vertexlist.=="noncoding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->coding"]),
+	layer(data[vertexlist.=="noncoding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->noncoding"]))
 
 # ╔═╡ 0502e056-3059-4941-84d0-fe7da897bc48
 data[vertexlist.=="coding",:]
@@ -72,5 +76,5 @@ data[vertexlist.=="coding",:]
 # ╠═02211a8a-87a4-4bd5-acf8-74cd27b902c6
 # ╠═5e6beb8d-47fc-4a1b-926e-ae17a51b2ee4
 # ╠═bd3d754d-9687-4ac2-bccc-b16fca7dfcf5
-# ╠═732e425b-6549-48c5-bf8c-e196d977356b
+# ╠═db25a7ae-26b0-40da-97d3-06edb70c70d9
 # ╠═0502e056-3059-4941-84d0-fe7da897bc48
