ab2a991262f6cb2ac947bda099cc836bc0249b29
osbornejr <joelwilliamrobertson@gmail.com>
2023-03-05 23:23:38 +1100

    updating pluto

--- pluto/typed_degree_distribution.jl
+++ pluto/typed_degree_distribution.jl
@@ -35,10 +35,10 @@ cwd = ENV["PWD"];
 	dev(path=cwd*"/packages/GraphletCounting")
 	dev(path=cwd*"/packages/GraphletAnalysis")
 	dev(path=cwd*"/packages/ProjectFunctions")
-	#using DataPreprocessing
+	using DataPreprocessing
 	using NetworkConstruction
 	using GraphletCounting
-	#using GraphletAnalysis
+	using GraphletAnalysis
 	using ProjectFunctions
 end
 
@@ -54,7 +54,7 @@ raw_counts,processed_counts,similarity_matrix,adj_matrix,network_counts,vertexli
 dd = GraphletCounting.typed_degree_distribution(vertexlist,edgelist);
 
 # ╔═╡ 5e6beb8d-47fc-4a1b-926e-ae17a51b2ee4
-data = DataFrame(coding = map(x->DefaultDict(0,x)["coding"],dd),noncoding = map(x->DefaultDict(0,x)["noncoding"],dd));
+plot_dd_data = DataFrame(coding = map(x->DefaultDict(0,x)["coding"],dd),noncoding = map(x->DefaultDict(0,x)["noncoding"],dd));
 
 # ╔═╡ db25a7ae-26b0-40da-97d3-06edb70c70d9
 bc = 50
@@ -62,16 +62,67 @@ bc = 50
 # ╔═╡ 53770902-0cee-4880-b480-e475e158a369
 # ╠═╡ show_logs = false
 plot(
-	layer(data[vertexlist.=="coding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["coding->coding"]),
-	layer(data[vertexlist.=="coding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["coding->noncoding"]),
-	layer(data[vertexlist.=="noncoding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->coding"]),
-	layer(data[vertexlist.=="noncoding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->noncoding"]),
+	layer(plot_dd_data[vertexlist.=="coding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["coding->coding"]),
+	layer(plot_dd_data[vertexlist.=="coding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["coding->noncoding"]),
+	layer(plot_dd_data[vertexlist.=="noncoding",:],x = "coding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->coding"]),
+	layer(plot_dd_data[vertexlist.=="noncoding",:],x = "noncoding",Geom.line,Stat.histogram(bincount=bc),color=["noncoding->noncoding"]),
 	Guide.title("Typed degree distribution"),
 	Guide.xlabel("degree"),
 	Guide.ylabel("frequency"))
 
 # ╔═╡ 0ed6a565-9abc-428b-bf0f-81be4a0b1f9c
-components = NetworkConstruction.network_components(adj_matrix)
+components = NetworkConstruction.network_components(adj_matrix) 
+
+# ╔═╡ 392062f3-e17f-4532-9337-692c229944a1
+ processed_data = DataPreprocessing.data_from_dataframe(processed_counts,"data")
+
+# ╔═╡ 96e7e7aa-ec6a-44fd-8914-70c3f44c771f
+dodgy_component_data = processed_data[components[2],:]
+
+# ╔═╡ 7e828b37-d757-4c23-bf21-aa821e776291
+processed_long_data = DataFrame(
+	names = repeat(processed_counts.transcript_id,length(2:99)),
+	var = stack(processed_counts[:,2:99])[:,1],
+	val = stack(processed_counts[:,2:99])[:,2]
+);
+
+# ╔═╡ 69a0ad82-e6e7-4832-b393-95ff5561722e
+processed_long_data_comp_2 = DataFrame(
+	names = repeat(processed_counts.transcript_id[components[2]],length(2:99)),
+	var = stack(processed_counts[components[2],2:99])[:,1],
+	val = stack(processed_counts[components[2],2:99])[:,2]
+);
+
+# ╔═╡ a854f969-4a58-4ef6-bfb5-0156650932e7
+raw_long_data_comp_2 = DataFrame(
+	names = repeat(raw_counts.transcript_id[components[2]],length(2:99)),
+	var = stack(raw_counts[components[2],2:99])[:,1],
+	val = stack(raw_counts[components[2],2:99])[:,2]
+);
+
+# ╔═╡ f179209c-cb71-437c-95c6-ba39513ee394
+Gadfly.plot(raw_long_data_comp_2,x=:var,y=:val,group=:names,Geom.line,Guide.xticks(label=false),Theme(key_position = :none))
+
+
+# ╔═╡ d651be50-9d95-49fc-bc70-dfc119947a6c
+map_component_back_to_raw = findall(x->x in processed_counts.transcript_id[components[2]],raw_counts.transcript_id)
+
+# ╔═╡ e6670e4e-e11a-4603-a4cb-fea6bc603933
+begin
+	test = zeros(Int,140)
+	for i in 1:140 
+		test[i] = length(filter(x->x == processed_counts.transcript_id[components[2]][i],raw_counts.transcript_id))
+	end
+end
+
+# ╔═╡ 72849afd-0f69-4b1b-ba2f-5d7567a6b682
+prob = processed_counts.transcript_id[components[2]][86]
+
+# ╔═╡ 0c47136e-c65c-421c-bdec-318c61a75282
+raw_counts[findall(raw_counts.transcript_id .== prob),:]
+
+# ╔═╡ a65a8563-ec0e-4627-8ca0-7271a03db258
+
 
 # ╔═╡ Cell order:
 # ╠═63f79e1a-b0d8-11ed-1669-a770b3844775
@@ -82,3 +133,14 @@ components = NetworkConstruction.network_components(adj_matrix)
 # ╠═db25a7ae-26b0-40da-97d3-06edb70c70d9
 # ╠═53770902-0cee-4880-b480-e475e158a369
 # ╠═0ed6a565-9abc-428b-bf0f-81be4a0b1f9c
+# ╠═392062f3-e17f-4532-9337-692c229944a1
+# ╠═96e7e7aa-ec6a-44fd-8914-70c3f44c771f
+# ╠═f179209c-cb71-437c-95c6-ba39513ee394
+# ╠═7e828b37-d757-4c23-bf21-aa821e776291
+# ╠═69a0ad82-e6e7-4832-b393-95ff5561722e
+# ╠═a854f969-4a58-4ef6-bfb5-0156650932e7
+# ╠═d651be50-9d95-49fc-bc70-dfc119947a6c
+# ╠═e6670e4e-e11a-4603-a4cb-fea6bc603933
+# ╠═72849afd-0f69-4b1b-ba2f-5d7567a6b682
+# ╠═0c47136e-c65c-421c-bdec-318c61a75282
+# ╠═a65a8563-ec0e-4627-8ca0-7271a03db258
