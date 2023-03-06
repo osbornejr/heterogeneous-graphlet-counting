0f029fde4ad8266e2362a07784e5d5ae5b8aa19d
osbornejr <joelwilliamrobertson@gmail.com>
2023-03-06 14:25:14 +1100

    updating pluto doc

--- pluto/typed_degree_distribution.jl
+++ pluto/typed_degree_distribution.jl
@@ -80,8 +80,32 @@ components = NetworkConstruction.network_components(adj_matrix)
 # ╔═╡ 96e7e7aa-ec6a-44fd-8914-70c3f44c771f
 dodgy_component_data = processed_data[components[2],:]
 
+# ╔═╡ df684d90-6453-41f8-b0b2-7ca47a4f9ce3
+#create binned counts to compare in plot (only for component 2)
+begin
+	binned_counts = processed_counts[components[2],:];
+	binned_counts[:,2:99] = NetworkConstruction.discretise(processed_data[components[2],:],nbins = 50);
+	binned_long_data = DataFrame(
+	names = repeat(binned_counts.transcript_id,length(2:99)),
+	var = stack(binned_counts[:,2:99])[:,1],
+	val = stack(binned_counts[:,2:99])[:,2]
+);
+	##plot showing bins across samples for each node in network. Coloured by two components in network
+Gadfly.plot(binned_long_data,x=:var,y=:val,group=:names,Geom.line,Guide.xticks(label=false),
+	Guide.xlabel("sample"),
+	Guide.ylabel("bin"),
+	Guide.title("All bin values across samples for nodes in network"),
+	Theme(grid_line_width=0mm));
+end
+
 # ╔═╡ 05cec146-5a7b-4bc6-8797-d7f89f5b8b8b
-comp_col = in.(1:2582,Ref(components[2]))
+comp_col = string.(in.(1:2582,Ref(components[2])).+1);
+
+# ╔═╡ 02d67238-533e-4c36-85c4-f828f130e340
+#comp_col[top_count] = "782";
+
+# ╔═╡ 7882c063-a58f-4747-a3b0-54c4a756bcf4
+top_count = findall(processed_data.==max(processed_data...))[1][1]
 
 # ╔═╡ 7e828b37-d757-4c23-bf21-aa821e776291
 processed_long_data = DataFrame(
@@ -92,6 +116,7 @@ processed_long_data = DataFrame(
 );
 
 # ╔═╡ f179209c-cb71-437c-95c6-ba39513ee394
+##plot showing counts across samples for each node in network. Coloured by two components in network
 Gadfly.plot(processed_long_data,x=:var,y=:val,group=:names,color= :component,Geom.line,Guide.xticks(label=false),
 	Guide.xlabel("sample"),
 	Guide.ylabel("count"),
@@ -107,20 +132,31 @@ processed_long_data_comp_2 = DataFrame(
 );
 
 # ╔═╡ c97ea9f4-c179-4b8e-9abe-d180b75e1abb
+##separate plot for 2nd component (if needed)
 Gadfly.plot(processed_long_data_comp_2,x=:var,y=:val,group=:names,Geom.line,Guide.xticks(label=false),
 	Guide.xlabel("sample"),
 	Guide.ylabel("count"),
 	Guide.title("2nd component (clique) processed counts"),
-	Theme(key_position = :none,grid_line_width=0mm))
+	Theme(key_position = :none,grid_line_width=0mm));
 
 # ╔═╡ d651be50-9d95-49fc-bc70-dfc119947a6c
-map_component_back_to_raw = findall(x->x in processed_counts.transcript_id[components[2]],raw_counts.transcript_id)
+map_component_back_to_raw = findall(x->x in processed_counts.transcript_id[components[2]],raw_counts.transcript_id);
 
 # ╔═╡ a854f969-4a58-4ef6-bfb5-0156650932e7
 raw_long_data_comp_2 = DataFrame(
 	names = repeat(raw_counts.transcript_id[map_component_back_to_raw],length(2:99)),
 	var = stack(raw_counts[map_component_back_to_raw,2:99])[:,1],
 	val = stack(raw_counts[map_component_back_to_raw,2:99])[:,2]
+);
+
+# ╔═╡ 6112be32-1cff-4eda-8a9c-b2bf350e1e0e
+map_processed_back_to_raw = findall(x->x in processed_counts.transcript_id,raw_counts.transcript_id);
+
+# ╔═╡ 67bd2eef-b716-41f0-89b0-4edc07c88948
+raw_long_data = DataFrame(
+	names = repeat(raw_counts.transcript_id[map_processed_back_to_raw],length(2:99)),
+	var = stack(raw_counts[map_processed_back_to_raw,:][:,2:99])[:,1],
+	val = stack(raw_counts[map_processed_back_to_raw,:][:,2:99])[:,2]
 );
 
 # ╔═╡ e6670e4e-e11a-4603-a4cb-fea6bc603933
@@ -132,6 +168,7 @@ begin
 end
 
 # ╔═╡ 72849afd-0f69-4b1b-ba2f-5d7567a6b682
+##node that is repeated in network!
 prob = processed_counts.transcript_id[components[2]][86]
 
 # ╔═╡ 0c47136e-c65c-421c-bdec-318c61a75282
@@ -158,12 +195,17 @@ yodel["ENST00000335426.4"]
 # ╠═392062f3-e17f-4532-9337-692c229944a1
 # ╠═96e7e7aa-ec6a-44fd-8914-70c3f44c771f
 # ╠═f179209c-cb71-437c-95c6-ba39513ee394
+# ╠═df684d90-6453-41f8-b0b2-7ca47a4f9ce3
 # ╠═c97ea9f4-c179-4b8e-9abe-d180b75e1abb
 # ╠═05cec146-5a7b-4bc6-8797-d7f89f5b8b8b
+# ╠═02d67238-533e-4c36-85c4-f828f130e340
+# ╠═7882c063-a58f-4747-a3b0-54c4a756bcf4
 # ╠═7e828b37-d757-4c23-bf21-aa821e776291
 # ╠═69a0ad82-e6e7-4832-b393-95ff5561722e
+# ╠═67bd2eef-b716-41f0-89b0-4edc07c88948
 # ╠═a854f969-4a58-4ef6-bfb5-0156650932e7
 # ╠═d651be50-9d95-49fc-bc70-dfc119947a6c
+# ╠═6112be32-1cff-4eda-8a9c-b2bf350e1e0e
 # ╠═e6670e4e-e11a-4603-a4cb-fea6bc603933
 # ╠═72849afd-0f69-4b1b-ba2f-5d7567a6b682
 # ╠═0c47136e-c65c-421c-bdec-318c61a75282
