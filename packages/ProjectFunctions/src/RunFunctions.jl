f5a73dd0d4308e8c4faf7edd70ce7a92ef658f92
Joel Robertson <joelwilliamrobertson@gmail.com>
2023-04-16 00:52:58 +1000

    fixing bug in component selection

--- packages/ProjectFunctions/src/RunFunctions.jl
+++ packages/ProjectFunctions/src/RunFunctions.jl
@@ -313,17 +313,21 @@ function network_construction(sample_counts::DataFrame)
             throw(ArgumentError("Threshold method $(params["network_construction"]["threshold_method"]) is not recognised."))
 
         end
-        ##form final adjacency matrix
+        ##form final adjacency matrix (remove zero nodes)
         adj_matrix = copy(pre_adj_matrix)
-        adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
-        adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=2).!=0),:]
+        
+        ##TODO HACK: remove dodgy second component from network (kinda assumes there are no zero nodes)
+        #get components
+        components = NetworkConstruction.network_components(pre_adj_matrix)
+        #get largest component
+        largest = findmax(length.(components))[2]
+        adj_matrix = pre_adj_matrix[components[largest],components[largest]]
+        
+        #old method-- just get all nonzero components
+        #adj_matrix = adj_matrix[:,vec(sum(pre_adj_matrix,dims=1).!=0)]
+        #adj_matrix = adj_matrix[vec(sum(pre_adj_matrix,dims=1).!=0),:]
 
 
-    ##TODO HACK: remove dodgy second component from network (kinda assumes there are no zero nodes)
-    #get components
-    components = NetworkConstruction.network_components(adj_matrix)
-    pre_adj_matrix = pre_adj_matrix[components[1],components[1]]
-    adj_matrix = adj_matrix[components[1],components[1]]  
 
         @info "Saving adjacency matrix to $adj_file"
         cache_save(adj_file,["adjacency_matrix"=>adj_matrix,"pre-adj_matrix"=>pre_adj_matrix,"components"=>components])
@@ -331,8 +335,12 @@ function network_construction(sample_counts::DataFrame)
 
 
     #Trim nodes with degree zero (and TODO HACK remove component 2 as well!
+    
 
-    network_counts = sample_counts[components[1],:][vec(sum(pre_adj_matrix,dims=2).!=0),:]
+
+    network_counts = sample_counts[components[largest],:]
+    #or to just get all non-zero components
+    #network_counts = sample_counts[vec(sum(pre_adj_matrix,dims=2).!=0),:]
     #maintain list of vertices in graph
     vertexlist = copy(network_counts[!,:transcript_type])     
     edgelist = NetworkConstruction.edgelist_from_adj(adj_matrix)
