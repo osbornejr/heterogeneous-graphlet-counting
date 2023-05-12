55b3f05cfdd0b93de7af9a28f30b2327b6fe62f9
Joel Robertson <joelwilliamrobertson@gmail.com>
2023-05-12 19:05:42 +1000

    adding pklutol doc for bio val

---
+++ pluto/biological_validation.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.19.25
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ da38df80-f09d-11ed-3d17-53c68078598e
+md"""
+# Biological validation
+"""
+
+# ╔═╡ 2f3728f9-2707-4ca7-ba4c-f52b10a8c3b9
+md"""
+## Entrez ids
+To find matches between our transcripts and either GO terms or KEGG pathways, we first need to map the Ensembl ids of our transcripts to Entrez gene ids.
+This can be done using the `biomaRt` package in `R`, but it is not necessarily straightforward; there is some ambiguity when matching transcript ids to gene ids.
+
+### Matching transcript ids to entrez gene ids
+
+
+### Matching gene ids to entrez gene ids
+"""
+
+# ╔═╡ ac76d04d-dcfb-443c-8682-191def877aa3
+md"""
+### Coverage statistics
+
+"""
+
+# ╔═╡ Cell order:
+# ╠═da38df80-f09d-11ed-3d17-53c68078598e
+# ╠═2f3728f9-2707-4ca7-ba4c-f52b10a8c3b9
+# ╠═ac76d04d-dcfb-443c-8682-191def877aa3
