c32f9b10a8a6a927633dd97bd36f61443dd8e71d
Joel Robertson <joelwilliamrobertson@gmail.com>
2023-05-15 15:24:17 +1000

    updating results script

--- scripts/cell-systems-results.jl
+++ scripts/cell-systems-results.jl
@@ -1,17 +1,11 @@
 using ProjectFunctions,CSV
 
-##bio validation table of top kegg terms associated with network
-top_terms = ProjectFunctions.biological_validation()[3]
-## calculate enrichment score (invert p-values on log scale)
-top_terms.enrichment_score = broadcast(x->-log(x),top_terms.P_DE) 
-## remove homo sapiens tail from every pathway (mention in caption)
-top_terms.Pathway = replace.(top_terms.Pathway," - Homo sapiens (human)"=>"")
-#rekmove other longwinded subcats
-top_terms.Pathway = replace.(top_terms.Pathway," - multiple diseases"=>"")
-top_terms.Pathway = replace.(top_terms.Pathway," - reactive oxygen species"=>"")
-top_terms.Pathway = replace.(top_terms.Pathway," and "=>"/")
+##load in information
+kegg_top_terms,go_top_terms = ProjectFunctions.biological_validation()
 
-tex = "\\begin{tikzpicture}
+function tex_enrichment_bar(top_names::Vector{String},,enrichment_scores::Vector{Float},outfile::String)
+
+        tex = "\\begin{tikzpicture}
         \\begin{axis}[
             xbar,
             xmin = 0,
@@ -22,16 +16,32 @@ tex = "\\begin{tikzpicture}
             xlabel = Enrichment score \$(-\\log(p))\$,
             symbolic y coords ={"
 ##add in names for y coords
-tex*= join(sort(top_terms,:enrichment_score).Pathway,",")*"},\n"
+#tex*= join(sort(kegg_top_terms,:enrichment_score).Pathway,",")*"},\n"
+tex*= join(reverse(top_names),",")*"},\n"
 tex*="ytick=data]\n"
 ##add coordinates for bars
 tex*= "\\addplot coordinates {"
-for i in eachrow(top_terms)
-    global tex*="($(i.enrichment_score),$(i.Pathway)) "
+for i in 1:length(top_names)
+    global tex*="($(enrichment_scores[i]),$(top_names[i])) "
 end
 tex*="};\n\\end{axis}\n\\end{tikzpicture}"
-write("output/share/bio_bar.tex",tex)
-    
+write(outfile,tex)
+end
 
+### KEGG enrichment
+##bio validation table of top kegg terms associated with network
+## calculate enrichment score (invert p-values on log scale)
+kegg_top_terms.enrichment_score = broadcast(x->-log(x),kegg_top_terms.P_DE) 
+## remove homo sapiens tail from every pathway (mention in caption)
+kegg_top_terms.Pathway = replace.(kegg_top_terms.Pathway," - Homo sapiens (human)"=>"")
+#rekmove other longwinded subcats
+kegg_top_terms.Pathway = replace.(kegg_top_terms.Pathway," - multiple diseases"=>"")
+kegg_top_terms.Pathway = replace.(kegg_top_terms.Pathway," - reactive oxygen species"=>"")
+kegg_top_terms.Pathway = replace.(kegg_top_terms.Pathway," and "=>"/")
 
+tex_enrichment_bar(kegg_top_terms.Pathway,kegg_top_terms.enrichment_score,"output/share/kegg_bar.tex")
+### GO enrichment
 
+go_top_terms.enrichment_score = broadcast(x->-log(x),go_top_terms.P_DE) 
+
+tex_enrichment_bar(go_top_terms.Pathway,go_top_terms.enrichment_score,"output/share/go_bar.tex")
