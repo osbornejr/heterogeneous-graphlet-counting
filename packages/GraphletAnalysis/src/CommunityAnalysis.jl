153580fec8557ead3806df970c1817a471b1ea46
Joel Robertson <joelwilliamrobertson@gmail.com>
2023-08-31 13:33:02 +1000

    adding docs to type function

--- packages/GraphletAnalysis/src/CommunityAnalysis.jl
+++ packages/GraphletAnalysis/src/CommunityAnalysis.jl
@@ -7,11 +7,18 @@ Find graphlet counts for each community subgraph in network.
 
 """
 function graphlet_counts_per_community(vertexlist::Vector{<:AbstractString},edgelist::Union{Array{Pair{Int,Int},1},Array{Pair,1}},community_groups::Vector{Int},graphlet_size::Int=3;run_method::String="serial",progress::Bool=false,recursive::Bool=true)
-   
+  
 
 end
 
 
+"""
+
+    types_per_community
+
+Find type distribution for each community subgraph in network.
+
+"""
 function types_per_community(vertexlist::Vector{<:AbstractString},community_groups::Vector{Int})
     types = unique(vertexlist)
     ##first print out percentage for whole network
