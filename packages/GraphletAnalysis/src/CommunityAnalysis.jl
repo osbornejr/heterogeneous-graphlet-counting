4fdbf690964517f8de580eff5c573db7fe175ceb
Joel Robertson <joelwilliamrobertson@gmail.com>
2023-09-14 23:45:09 +1000

    per commun vertex list

--- packages/GraphletAnalysis/src/CommunityAnalysis.jl
+++ packages/GraphletAnalysis/src/CommunityAnalysis.jl
@@ -8,9 +8,15 @@ Find graphlet counts for each community subgraph in network.
 """
 function graphlet_counts_per_community(vertexlist::Vector{<:AbstractString},edgelist::Union{Array{Pair{Int,Int},1},Array{Pair,1}},community_groups::Vector{Int},graphlet_size::Int=3;run_method::String="serial",progress::Bool=false,recursive::Bool=true)
     
+    if(length(vertexlist)!==length(community_groups))
+        throw(ArgumentError("vector of community groups must be same length as vertex type list"))
+    end
+    
     comms = unique(community_groups)
     for c in comms 
- GraphletCounting.count_graphlets(vertexlist,edgelist,graphlet_size,run_method="distributed-old")           
+        ##get sub vertexlist (using index as ref)
+        c_v = findall(.==(c),community_groups)
+        GraphletCounting.count_graphlets(vertexlist,edgelist,graphlet_size,run_method=run_method)           
 
 
     end
