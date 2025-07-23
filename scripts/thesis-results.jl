f4c5dbc2ff6f3e520bd20a9ab32ab51048936e82
osbornejr <joelwilliamrobertson@gmail.com>
2025-07-23 17:25:09 +1000

    adding script to generate makie plots of preprocessing output

--- scripts/thesis-results.jl
+++ scripts/thesis-results.jl
@@ -1,6 +1,11 @@
-using GLMakie
+using GLMakie,StatsBase
 using ProjectFunctions
-raw_counts,round_counts,vst_counts,clean_counts,norm_counts,sample_counts = get_preprocessed_data()
+raw_counts,round_counts,vst_counts,clean_counts,norm_counts,processed_counts = get_preprocessed_data("config/run-files/mayank-merged.yaml")
+var_data = vec(var(data_from_dataframe(norm_counts),dims=2 ))
+
+#histogram to see where coding and noncoding is represented in variance data
+bins = Int.(floor.(var_data.*10)).+1
+barplot(bins,repeat([1],length(var_data)),stack=bins,color=replace(norm_counts.transcript_type,"coding"=>1,"noncoding"=>2))
 
 
 
